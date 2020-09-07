4e139289b89a2915099f4915f9269d5c420ff364
BadFrameRate <rohatgi.aseem03@gmail.com>
2020-09-07 22:23:05 +0530

    def new var properly; def names of means properly

--- pollutantmean.R
+++ pollutantmean.R
@@ -2,12 +2,12 @@ pollutantmean<-function(directory,pollutant,id=1:332){
   if(directory!="specdata") print("Wrong folder")
   else{
     all_files<-list.files(directory)
-    required_mean<-numeric(id)
-    headers<-character(id)
+    required_mean<-numeric(length(id))
+    headers<-character(length(id))
     for(i in id){
       openfile<-read.csv(paste(directory,all_files[i],sep="/"))
       required_mean[i]<-mean(openfile[,pollutant],na.rm=T)
-      headers[i]<-openfile[i]
+      headers[i]<-all_files[i]
     }
     names(required_mean)<-headers
     required_mean
