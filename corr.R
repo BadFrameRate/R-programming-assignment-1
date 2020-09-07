67003c9c4cb1a2595d060e72204dba3a4e2c3f48
BadFrameRate <rohatgi.aseem03@gmail.com>
2020-09-07 23:31:39 +0530

    learned the use of "use" option in cor() and made the function in one go

---
+++ corr.R
@@ -0,0 +1,17 @@
+corr<-function(directory,threshold=0){
+  if(directory!="specdata") print("Wrong folder")
+  else{
+    all_files<-list.files(directory)
+    n_obs<-complete(directory)
+    above_thresh<-n_obs$id[n_obs$obs>=threshold]
+    corr<-numeric(length(above_thresh))
+    headers<-character(length(above_thresh))
+    for(i in 1:length(above_thresh)){
+      openfile<-read.csv(paste(directory,all_files[above_thresh[i]],sep="/"))
+      corr[i]<-cor(openfile[,2],openfile[,3],use="complete.obs")
+      headers[i]<-above_thresh[i]
+    }
+    names(corr)<-headers
+    corr
+  }
+}
