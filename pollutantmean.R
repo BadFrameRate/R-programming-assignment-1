d68bca44497c153a7cab0b105b72122fda069738
BadFrameRate <rohatgi.aseem03@gmail.com>
2020-09-07 22:12:45 +0530

    def new var; performed req calc

--- pollutantmean.R
+++ pollutantmean.R
@@ -2,9 +2,13 @@ pollutantmean<-function(directory,pollutant,id=1:332){
   if(directory!="specdata") print("Wrong folder")
   else{
     all_files<-list.files(directory)
+    required_mean<-numeric(id)
+    headers<-character(id)
     for(i in id){
       openfile<-read.csv(paste(directory,all_files[i],sep="/"))
+      required_mean[i]<-mean(openfile[,pollutant],na.rm=T)
+      headers[i]<-openfile[i]
     }
-    head(openfile)
+    required_mean
   }
 }
