f68ef14a03266101aed9d9861592f6db322f4fef
BadFrameRate <rohatgi.aseem03@gmail.com>
2020-09-07 22:58:10 +0530

    coreected calc of observations

--- complete.R
+++ complete.R
@@ -5,7 +5,8 @@ complete<-function(directory,id=1:332){
     obs<-integer(length(id))
     for(i in id){
       openfile<-read.csv(paste(directory,all_files[i],sep="/"))
-      obs[i]<-length(openfile[,1])
+      na_removal<-openfile[,2][!is.na(openfile[,2])]
+      obs[i]<-length(na_removal)
     }
     output<-data.frame(id,obs)
     output
