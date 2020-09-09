1e452479552eb67267e77f2b105bceb03a099464
BadFrameRate <rohatgi.aseem03@gmail.com>
2020-09-09 18:59:53 +0530

    now calc complete obs that are complete for both sulfate & nitrate

--- complete.R
+++ complete.R
@@ -5,7 +5,7 @@ complete<-function(directory,id=1:332){
     obs<-integer(length(id))
     for(i in seq_along(id)){
       openfile<-read.csv(paste(directory,all_files[id[i]],sep="/"))
-      na_removal<-openfile[,2][!is.na(openfile[,2])]
+      na_removal<-openfile[,2][!is.na(openfile[,2])&!is.na(openfile[,3])]
       obs[i]<-length(na_removal)
     }
     output<-data.frame(id,obs)
