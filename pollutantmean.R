214bd23d003a6ff96c0175a02684bfa12b89650b
BadFrameRate <rohatgi.aseem03@gmail.com>
2020-09-09 18:16:32 +0530

    changed the calc variables from arrays to single variables; changed calc code accordingly

--- pollutantmean.R
+++ pollutantmean.R
@@ -2,13 +2,13 @@ pollutantmean<-function(directory,pollutant,id=1:332){
   if(directory!="specdata") print("Wrong folder")
   else{
     all_files<-list.files(directory)
-    temp_sum<-numeric(length(id))
-    temp_obs<-numeric(length(id))
+    temp_sum<-0
+    temp_obs<-0
     for(i in id){
       openfile<-read.csv(paste(directory,all_files[i],sep="/"))
-      temp_sum[i]<-sum(openfile[,pollutant],na.rm=T)
-      temp_obs[i]<-sum(!is.na(openfile[,pollutant]))
+      temp_sum<-sum(temp_sum,openfile[,pollutant],na.rm=T)
+      temp_obs<-sum(temp_obs,!is.na(openfile[,pollutant]))
     }
-    sum(temp_sum)/sum(temp_obs)
+    temp_sum/temp_obs
   }
 }
