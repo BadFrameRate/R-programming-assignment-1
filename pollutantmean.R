2822cea88350a3b4099ee79120fd8ddf447c4a5d
BadFrameRate <rohatgi.aseem03@gmail.com>
2020-09-09 18:05:48 +0530

    prevously calc file-wise mean; now calc overall mean

--- pollutantmean.R
+++ pollutantmean.R
@@ -2,14 +2,13 @@ pollutantmean<-function(directory,pollutant,id=1:332){
   if(directory!="specdata") print("Wrong folder")
   else{
     all_files<-list.files(directory)
-    required_mean<-numeric(length(id))
-    headers<-character(length(id))
+    temp_sum<-numeric(length(id))
+    temp_obs<-numeric(length(id))
     for(i in id){
       openfile<-read.csv(paste(directory,all_files[i],sep="/"))
-      required_mean[i]<-mean(openfile[,pollutant],na.rm=T)
-      headers[i]<-all_files[i]
+      temp_sum[i]<-sum(openfile[,pollutant],na.rm=T)
+      temp_obs[i]<-sum(!is.na(openfile[,pollutant]))
     }
-    names(required_mean)<-headers
-    required_mean
+    sum(temp_sum)/sum(temp_obs)
   }
 }
