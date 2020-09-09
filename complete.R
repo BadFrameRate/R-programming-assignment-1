6eff0f59a689e78bfd7facd825f0c50975ccf5be
BadFrameRate <rohatgi.aseem03@gmail.com>
2020-09-09 18:26:24 +0530

    modified the for loop to work with non-continuous vectors

--- complete.R
+++ complete.R
@@ -3,8 +3,8 @@ complete<-function(directory,id=1:332){
   else{
     all_files<-list.files(directory)
     obs<-integer(length(id))
-    for(i in id){
-      openfile<-read.csv(paste(directory,all_files[i],sep="/"))
+    for(i in seq_along(id)){
+      openfile<-read.csv(paste(directory,all_files[id[i]],sep="/"))
       na_removal<-openfile[,2][!is.na(openfile[,2])]
       obs[i]<-length(na_removal)
     }
