52d2994741426dcb21b34288f15a95aabe04cb6b
BadFrameRate <rohatgi.aseem03@gmail.com>
2020-09-09 18:42:51 +0530

    changed >= to > before 'threshold'

--- corr.R
+++ corr.R
@@ -3,7 +3,7 @@ corr<-function(directory,threshold=0){
   else{
     all_files<-list.files(directory)
     n_obs<-complete(directory)
-    above_thresh<-n_obs$id[n_obs$obs>=threshold]
+    above_thresh<-n_obs$id[n_obs$obs>threshold]
     corr<-numeric(length(above_thresh))
     headers<-character(length(above_thresh))
     for(i in 1:length(above_thresh)){
