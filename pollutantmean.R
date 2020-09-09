8d69bff85daf2e49a44e16a63ba05ffcaa8a3768
BadFrameRate <rohatgi.aseem03@gmail.com>
2020-09-09 18:25:35 +0530

    modifie the for loop to work with non-ontinuous vectors

--- pollutantmean.R
+++ pollutantmean.R
@@ -4,8 +4,8 @@ pollutantmean<-function(directory,pollutant,id=1:332){
     all_files<-list.files(directory)
     temp_sum<-0
     temp_obs<-0
-    for(i in id){
-      openfile<-read.csv(paste(directory,all_files[i],sep="/"))
+    for(i in seq_along(id)){
+      openfile<-read.csv(paste(directory,all_files[id[i]],sep="/"))
       temp_sum<-sum(temp_sum,openfile[,pollutant],na.rm=T)
       temp_obs<-sum(temp_obs,!is.na(openfile[,pollutant]))
     }
