638a1f7cc8eada1cdd9225fd149328649f074c2c
BadFrameRate <rohatgi.aseem03@gmail.com>
2020-09-09 19:11:06 +0530

    added condition to return empty num vector if threshold condition is not met

--- corr.R
+++ corr.R
@@ -3,15 +3,16 @@ corr<-function(directory,threshold=0){
   else{
     all_files<-list.files(directory)
     n_obs<-complete(directory)
-    above_thresh<-n_obs$id[n_obs$obs>threshold]
-    corr<-numeric(length(above_thresh))
-    headers<-character(length(above_thresh))
-    for(i in 1:length(above_thresh)){
-      openfile<-read.csv(paste(directory,all_files[above_thresh[i]],sep="/"))
-      corr[i]<-cor(openfile[,2],openfile[,3],use="pairwise.complete.obs")
-      headers[i]<-above_thresh[i]
+    above_thresh<-as.numeric(n_obs$id[n_obs$obs>threshold])
+    if(length(above_thresh)==0) above_thresh
+    else {
+      corr<-numeric(length(above_thresh))
+      for(i in 1:length(above_thresh)){
+        openfile<-read.csv(paste(directory,all_files[above_thresh[i]],sep="/"))
+        corr[i]<-cor(openfile[,2],openfile[,3],use="pairwise.complete.obs")
+      }
+      names(corr)<-above_thresh
+      corr
     }
-    names(corr)<-headers
-    corr
   }
 }
